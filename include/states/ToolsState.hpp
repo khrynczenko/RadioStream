89f8273e182825eea3e8569fc9d3c58e2ad2b13b
JenioPY <jeniopy@gmail.com>
2018-04-23 21:11:00 +0200

    Issue 8 somewhat resolved.

--- include/states/ToolsState.hpp
+++ include/states/ToolsState.hpp
@@ -5,6 +5,7 @@
 #include <nana/gui/widgets/button.hpp>
 #include <nana/gui/widgets/combox.hpp>
 #include <nana/gui/place.hpp>
+#include <nana/gui/widgets/spinbox.hpp>
 
 class ToolsState : public State
 {
@@ -16,7 +17,10 @@ private:
 private:
 	std::string get_language_iso_identifier(const std::string& language) const;
 	nana::place container_;
+    nana::label language_label_;
 	nana::combox language_choices_;
+    nana::label found_stations_limit_;
+    nana::spinbox found_stations_values_;
 	nana::button back_button_;
 	nana::button apply_button_;
 };
