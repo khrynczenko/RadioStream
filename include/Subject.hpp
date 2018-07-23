12081527af3b8a94459a52395d6ec930eb11de2a
JenioPY <jeniopy@gmail.com>
2018-07-23 22:18:33 +0200

    Added missing files to cmake and project.

---
+++ include/Subject.hpp
@@ -0,0 +1,18 @@
+#ifndef SUBJECT_HPP
+#define SUBJECT_HPP
+
+#include "Observer.hpp"
+#include <vector>
+#include <memory>
+
+
+class Subject
+{
+public:
+	void attach(std::shared_ptr<Observer> observer);
+	void notify(const std::any& data, radiostream::Event e);
+private:
+	std::vector<std::shared_ptr<Observer>> observers_;
+};
+
+#endif
