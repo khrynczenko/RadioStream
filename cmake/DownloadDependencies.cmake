75ace92e2a41ee8898f49b27d60bf9e5a9e9a0ea
Krzysztof Hrynczenko <jeniopy@gmail.com>
2019-10-13 22:15:52 +0200

    Added support for AAC

--- cmake/DownloadDependencies.cmake
+++ cmake/DownloadDependencies.cmake
@@ -54,6 +54,7 @@ if(WIN32)
     set(BASS_URL "http://us.un4seen.com/files/bass24.zip")
 elseif(UNIX)
     set(BASS_URL "http://us.un4seen.com/files/bass24-linux.zip")
+    set(BASS_AAC_URL "http://us.un4seen.com/files/z/2/bass_aac24-linux.zip")
 endif()
 
 FetchContent_Declare(
@@ -64,8 +65,23 @@ FetchContent_Declare(
         INSTALL_COMMAND ""
         TEST_COMMAND ""
 )
+
+if(UNIX)
+FetchContent_Declare(
+        bass_aac
+        URL ${BASS_AAC_URL}
+        UPDATE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        TEST_COMMAND ""
+)
+endif()
+
 FetchContent_Populate(nana)
 FetchContent_Populate(poco)
 FetchContent_Populate(json)
 FetchContent_Populate(bass)
 FetchContent_Populate(clip)
+if(UNIX)
+    FetchContent_Populate(bass_aac)
+endif()
